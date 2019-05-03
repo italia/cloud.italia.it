809d30802cfb130834d417f2e20984cd8b3c97f7
Francesco Zaia <francesco.zaia@gmail.com>
2019-05-03 11:08:43 +0200

    Theme naming update

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ settings_file_name = 'cloud-italia'
 # -- No need to change below here
 
 import sys, os
-docs_italia_theme = __import__("docs-italia-theme")
+docs_italia_theme = __import__("docs_italia_theme")
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
@@ -49,16 +49,16 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
-    'docs-italia-theme',
+    'docs_italia_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+# source_parsers = {
+#    '.md': CommonMarkParser,
+# }
 
 source_suffix = ['.rst', '.md']
 
@@ -113,7 +113,7 @@ def setup(app):
 
 
 # -- Options for HTML output ----------------------------------------------
-html_theme = 'docs-italia-theme'
+html_theme = 'docs_italia_theme'
 
 html_theme_path = [docs_italia_theme.get_html_theme_path()]
 
@@ -121,7 +121,7 @@ html_theme_path = [docs_italia_theme.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    # This option can be used with docs-italia-theme to customise how the versions "badge" is shown:
+    # This option can be used with docs_italia_theme to customise how the versions "badge" is shown:
     # 'False': default (alabaster) badge | 'True': custom (italia) badge
     'custom_versions_badge': 'True',
 }
@@ -131,7 +131,7 @@ html_theme_options = {
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    html_theme = 'docs-italia-theme'
+    html_theme = 'docs_italia_theme'
     #html_theme_path = ["themes", ]
 else:
     # Override default css to get a larger width for ReadTheDoc build
